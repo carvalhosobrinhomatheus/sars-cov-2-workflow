516a21a590a539d521ab98872e3d68fc79fa422e
Matheus de Carvalho Sobrinho <carvalhosobrinhomatheus@gmail.com>
2020-05-25 20:24:19 -0300

    [FIX] - Refactor dir structure

--- workflow/execution.sh
+++ workflow/execution.sh
@@ -7,12 +7,11 @@ for counter in $(seq 1 $rounds); do
     echo "####### Round $counter/$rounds";
     execs='SRR11178050 SRR11178051 SRR11178052 SRR11178053 SRR11178054 SRR11178055 SRR11178056 SRR11178057'
     for i in $execs; do
-        # datetime=$(date +"%m-%d-%Y-%H-%M-%S")
-        # dstat -C -D -N -m -r -y -g -i -l -p -s -t -T --noheaders --output dstat/$i_$datetime.csv >> dstat/$i_$datetime.txt &
-        # echo '\n### sh ./execute.sh' $1 $i
-        # (time sh ./execute.sh $1 $i) > log/$i_$datetime.log 2> time/$i_$datetime.log
-        # kill -15 `pgrep -f dstat`
-        echo $i;
+        datetime=$(date +"%m-%d-%Y-%H-%M-%S")
+        dstat -C -D -N -m -r -y -g -i -l -p -s -t -T --noheaders --output dstat/$i_$datetime.csv >> dstat/$i_$datetime.txt &
+        echo '\n### sh ./execute.sh' $1 $i
+        (time sh ./execute.sh $1 $i) > log/$i_$datetime.log 2> time/$i_$datetime.log
+        kill -15 `pgrep -f dstat`
     done
     (echo "["$ip"]-Excecution number["$counter"] - SRR - ["$execs"]") >> log/general_log__$general_datetime.log
 done
