01e7130bf6c1d41b818614a3cd3a06f235e4ef0e
05410994183 <matheus.sobrinho@redes.unb.br>
2020-05-26 12:09:49 -0300

    [ADD] - Trocado referência de nome para arquivos de log

--- workflow/execution.sh
+++ workflow/execution.sh
@@ -8,11 +8,12 @@ for counter in $(seq 1 $rounds); do
     execs='SRR11178050 SRR11178051 SRR11178052 SRR11178053 SRR11178054 SRR11178055 SRR11178056 SRR11178057'
     for i in $execs; do
         datetime=$(date +"%m-%d-%Y-%H-%M-%S")
-        dstat -C -D -N -m -r -y -g -i -l -p -s -t -T --noheaders --output dstat/$i_$datetime.csv >> dstat/$i_$datetime.txt &
+        name=AWS_$i_$datetime
+        dstat -C -D -N -m -r -y -g -i -l -p -s -t -T --noheaders --output dstat/$name.csv >> dstat/$name.txt &
         echo '\n### sh ./execute.sh' $1 $i
-        (time sh ./execute.sh $1 $i) > log/$i_$datetime.log 2> time/$i_$datetime.log
+        (time sh ./execute.sh $1 $i) > log/$name.log 2> time/$name.log
         kill -15 `pgrep -f dstat`
     done
-    (echo "["$ip"]-Excecution number["$counter"] - SRR - ["$execs"]") >> log/general_log__$general_datetime.log
+    (echo "["$ip"]-Excecution number["$counter"] - SRR - ["$execs"]") >> log/AWS_general_log__$general_datetime.log
 done
 
